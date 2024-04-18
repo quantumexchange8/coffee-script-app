4f4e1ee46166ed4cef51b217dca123ed7e13ba97
ZuffyX <hanafirohani.2902@gmail.com>
2024-04-18 17:47:53 +0800

    add pushReplacement method

---
+++ lib/helper/constant/method.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Future pushReplacement(BuildContext context,
+    {required Widget nextPage, Offset? begin}) async {
+  return await Navigator.pushReplacement(context, PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return SlideTransition(
+          position: animation.drive(
+              Tween(begin: begin ?? const Offset(1.0, 0), end: Offset.zero)
+                  .chain(
+            CurveTween(curve: Curves.linear),
+          )),
+          child: nextPage);
+    },
+  ));
+}
